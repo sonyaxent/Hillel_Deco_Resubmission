92e5fe3ee4399cc53b23616453c04b69843460ca
sonyaxent <sonyaxent@gmail.com>
2022-07-02 22:23:36 +0200

    new_commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import functools
-
+import sys
+from datetime import time
 
 import requests
 from memory_profiler import memory_usage
@@ -10,9 +11,8 @@ def profile_memory(msg='Memory info'):
         @functools.wraps(f)
         def deco(*args, **kwargs):
             result = f(*args, **kwargs)
-            result_to_memorize = tuple(result)
-            mem = memory_usage(proc=result_to_memorize)
-            print(msg, f'({f.__name__}):  {mem}')
+            mem = memory_usage((f, args, kwargs))
+            print(msg, f'({f.__name__}):  {max(mem)}')
             return result
 
         return deco
@@ -25,24 +25,39 @@ def cache_lfu(max_limit=100):
         @functools.wraps(f)
         def deco(*args, **kwargs):
             cache_key = (args, tuple(kwargs.items()))
-            deco._cache[cache_key]["counter"] = 1
             if cache_key in deco._cache:
                 deco._cache[cache_key]["counter"] += 1
-                return deco._cache[cache_key]
+                return deco._cache[cache_key]["result"]
 
-            result = f(*args, **kwargs)
+
 
             if len(deco._cache) >= max_limit:
                 del deco._cache[min(deco._cache, key=lambda cache_key: deco._cache[cache_key]["counter"])]
+
+            result = f(*args, **kwargs)
+
+            deco._cache[cache_key]["counter"] = 1
             deco._cache[cache_key]["result"] = result
+
             return result
+
         deco._cache = {}
         return deco
     return internal
 
+def profile(msg='Elapsed time', file=sys.stdout):
+    def internal(f):
+        @functools.wraps(f)
+        def deco(*args, **kwargs):
+            start = time.time()
+            result = f(*args, **kwargs)
+            print(msg, f'({f.__name__}): {time.time() - start}s', file=file)
+            return result
+        return deco
+    return internal
 
-
-@cache_lfu(max_limit=30)
+@cache_lfu
+@profile
 @profile_memory
 def fetch_url(url, first_n=120):
     """Fetch a given url"""
